7317b6a8e4cd526833bf6d64f1baf8914f7bab09
Tim Mui <timmui123@gmail.com>
2016-10-22 03:29:39 -0400

    Add ESlint and .env

---
+++ config.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+if (process.env.NODE_ENV !== 'production') {
+    // Load .env file
+    require('dotenv').config();
+}
+
+module.exports = {
+    port: process.env.port || process.env.PORT || 3978,
+    environment: process.env.NODE_ENV || 'development',
+    apiVersion: pkg.version,
+    MSAppId: process.env.MICROSOFT_APP_ID,
+    MSAppPassword: process.env.MICROSOFT_APP_PASSWORD,
+    MSTextAnalyticsKey: process.env.MICROSOFT_TEXT_ANALYTICS_KEY,
+};
