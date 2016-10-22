ee53f1c89ee72934e5f7dc47e92a524396ca4d1a
AdrianMachado <amachado@uwaterloo.ca>
2016-10-22 10:26:25 -0400

    LUIS Connection
    LUIS connection established, basic conversation example added.

--- app.js
+++ app.js
@@ -25,13 +25,25 @@ server.post('/api/messages', connector.listen());
 
 // Static root page
 server.get('/', (req, res) => {
-    res.send(200, `API Version ${config.apiVersion} currently running.`);
+    res.send(200, 'API Version ${config.apiVersion} currently running.');
 });
+
+// LUIS Setup
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=6bfcb96e-0dbb-443d-8c75-36224da1b80f&subscription-key=70c5729403d64977871078f0c34e46ac';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] })
+bot.dialog('/', dialog);
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
+dialog.matches('CreateAccount', [
+    (session, args, next) => {
+        var accountAction = builder.EntityRecognizer.findEntity(args.entities, 'AccountAction');
+        var accountType = builder.EntityRecognizer.findEntity(args.entities, 'AccountType');
+        session.send('Receieved Account Action and Type', accountAction, accountType);
+    }
+]);
 
-bot.dialog('/', (session) => {
-    session.send('Hello World');
-});
+dialog.onDefault(builder.DialogAction.send('I\'m sorry, I didn\'t quite catch that.'));
+
