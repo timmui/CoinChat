801779a82c5f5000ab40946beefd753ad92dbe5b
Rush <rush.shahani@gmail.com>
2016-10-22 23:48:59 -0400

    Linter

--- api_wrapper.js
+++ api_wrapper.js
@@ -14,12 +14,12 @@ function createAccount(name, type, cb) {
         }
   }
 request(options, function (error, response, body) {
-  if (!error && response.statusCode >= 200) {
+  if (!error && response.statusCode == 201) {
     console.log(body)
-    cb("Account Successfully Created")
+    cb('Account Successfully Created')
   }
   else {
-    cb("Could not create account. Please try again")
+    cb('Could not create account. Please try again')
   }
 })};
 
@@ -27,17 +27,17 @@ exports.createAccount = createAccount;
 
 
 function getAccounts(type,cb){
-  request("http://api.reimaginebanking.com/customers/580ba86e360f81f104544d31/accounts?key="+config.CapitalOneKey
+  request('http://api.reimaginebanking.com/customers/580ba86e360f81f104544d31/accounts?key='+config.CapitalOneKey
   , function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var jsonResponse = JSON.parse(body);
       returnString = '';
       for (var i = 0; i < jsonResponse.length; i++) {
-        if (jsonResponse[i].type != type) continue;
-        var comma = ',';
-        if (i==jsonResponse.length-1) comma = '';
-        returnString = returnString + jsonResponse[i].type + " (" + jsonResponse[i].nickname + ") "
-        + '$' + jsonResponse[i].balance.toFixed(2) + comma + "\n\n";
+        if (jsonResponse[i].type !== type) continue;
+        var delimiter = ',';
+        if (i==jsonResponse.length-1) delimiter = '';
+        returnString = returnString + jsonResponse[i].type + ' (' + jsonResponse[i].nickname + ')'
+        + '$' + jsonResponse[i].balance.toFixed(2) + delimiter + '\n\n';
       }
       return cb(returnString);
     }})};
@@ -48,12 +48,12 @@ function getAccounts(type,cb){
     request('http://api.reimaginebanking.com/atms?lat=38.9072&lng=-77.1753&rad=5&key='+config.CapitalOneKey, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var jsonResponse = JSON.parse(body).data;
-        console.log("The nearest ATM's are: ")
-        str = '';
+        console.log('The nearest ATMs are: ')
+        var str = '';
         for (var i = 0; i < 4; i++) {
           str = str + (i+1) + ') ' + jsonResponse[i].name + ' , ';
-          str = str + ("Address: " + jsonResponse[i].address.street_number + ' ' + jsonResponse[i].address.street_name + ", " + jsonResponse[i].address.city) + ', ';
-          str = str + ("Hours: " + jsonResponse[i].hours[0]) + '\n\n';
+          str = str + ('Address: ' + jsonResponse[i].address.street_number + ' ' + jsonResponse[i].address.street_name + ', ' + jsonResponse[i].address.city) + ', ';
+          str = str + ('Hours: ' + jsonResponse[i].hours[0]) + '\n\n';
         }
         return cb(str);
       }})}
